27998519b2a8fcb913f7726e54b1ad44429a7e1d
Anthony Sapp <anthony.sapp@campjefferson.com>
2016-10-21 16:56:31 -0400

    fixed audiomanager

--- core/SoundManager.ts
+++ core/SoundManager.ts
@@ -1,25 +1,28 @@
-import {PIXIApplication} from '../core/PIXIApplication';
-import {State} from '../display/State';
+import { PIXIApplication } from '../core/PIXIApplication';
+import { State } from '../display/State';
 
 export class SoundManager {
     private app: PIXIApplication;
     private lookup: { [id: string]: Howl } = {};
 
+    private _bgTrack: Howl = null;
+
     constructor() {
         this.app = PIXIApplication.getInstance() as PIXIApplication;
     }
 
-    public add(id: string, howl: Howl):Howl {
+    public add(id: string, howl: Howl): Howl {
         this.lookup[id] = howl;
         return howl;
     }
 
     public play(id: string, marker: string = null): Howl {
         if (marker) {
-            return this.lookup[id].play(marker);
+            this.lookup[id].play(marker);
         } else {
-            return this.lookup[id].play();
+            this.lookup[id].play();
         }
+        return this.lookup[id];
     }
 
     public loop(id: string): Howl {
@@ -27,11 +30,29 @@ export class SoundManager {
         return this.lookup[id];
     }
 
-    public pause(id: string, marker: string = null):Howl {
+    public pause(id: string, marker: string = null): Howl {
         if (marker) {
-            return this.lookup[id].pause(marker);
+            this.lookup[id].pause(marker);
         } else {
-            return this.lookup[id].pause();
+            this.lookup[id].pause();
         }
+        return this.lookup[id];
+    }
+
+    public setBgTrack(id: string): Howl {
+        this._bgTrack = this.play(id);
+        return this._bgTrack;
+    }
+
+    public crossFadeBGTrack(to: string, time: number = 300): Howl {
+        const oldBgTrack = this._bgTrack;
+        this._bgTrack.fade(this._bgTrack.volume(), 0, time);
+        this._bgTrack.on('fade', (id) => {
+            oldBgTrack.stop();
+        });
+        const newBgTrack = this.play(to);
+        newBgTrack.fade(0, newBgTrack.volume(), time);
+        this._bgTrack = newBgTrack
+        return newBgTrack;
     }
 }
